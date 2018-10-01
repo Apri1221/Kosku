a48e7619f7e18ce96a15c4a2ac8805e25f633a33
apri <apriyantotobing8@gmail.com>
2018-10-01 17:30:05 +0700

    Login lancar gan

--- Tugas/Form1.cs
+++ Tugas/Form1.cs
@@ -7,11 +7,18 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using MySql.Data.MySqlClient;
 
 namespace Tugas
 {
     public partial class Form1 : Form
     {
+        static string connectionString = "datasource=127.0.0.1;port=3306;username=root;password=apriyanto12;database=sistem_kos;SslMode=none";
+
+        MySqlConnection databaseConnection = new MySqlConnection(connectionString);
+        MySqlCommand commandDatabase;
+        MySqlDataReader reader;
+
         public Form1()
         {
             InitializeComponent();
@@ -19,7 +26,32 @@ namespace Tugas
 
         private void button1_Click(object sender, EventArgs e)
         {
-            Form2 main = new Form2();
+            
+            try
+            {
+                String query = "SELECT * FROM tabel_admin WHERE username = '" + textBox1.Text + "' AND password = '" + textBox2.Text + "'";
+                commandDatabase = new MySqlCommand(query, databaseConnection);
+                commandDatabase.CommandTimeout = 60;
+                databaseConnection.Open();
+                reader = commandDatabase.ExecuteReader();
+
+                if (reader.HasRows)
+                {
+                    this.Hide();
+                    Form2 main = new Form2();
+                    main.Show();
+                }
+                else
+                {
+                    MessageBox.Show("Tidak ada user yang dimaksud");
+                }
+
+                databaseConnection.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
         }
     }
 }
--- Tugas/Program.cs
+++ Tugas/Program.cs
@@ -16,7 +16,7 @@ namespace Tugas
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form2());
+            Application.Run(new Form1());
         }
     }
 }
