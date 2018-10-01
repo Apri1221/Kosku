4d77280b760af2dc9ecf3f54ce44a6eaf3f67289
apri <apriyantotobing8@gmail.com>
2018-10-01 16:49:22 +0700

    ok3

--- Tugas/Form1.Designer.cs
+++ Tugas/Form1.Designer.cs
@@ -71,7 +71,7 @@
             // 
             this.button1.Location = new System.Drawing.Point(91, 198);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.Size = new System.Drawing.Size(75, 38);
             this.button1.TabIndex = 4;
             this.button1.Text = "Masuk";
             this.button1.UseVisualStyleBackColor = true;
@@ -80,14 +80,14 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(264, 322);
+            this.ClientSize = new System.Drawing.Size(264, 292);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.textBox2);
             this.Controls.Add(this.textBox1);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "Login";
             this.ResumeLayout(false);
             this.PerformLayout();
 
